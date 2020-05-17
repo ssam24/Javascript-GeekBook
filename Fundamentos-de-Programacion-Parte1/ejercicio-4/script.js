d02a75730e49bcf3684033371ca17f08185b5f2f
samuel crespo <crespoalzatesamuel@gmail.com>
2020-05-17 13:00:17 -0500

    realizo ejercicio 4

---
+++ Fundamentos-de-Programacion-Parte1/ejercicio-4/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Obtenemos el elemento con el que queremos trabajar usando document.querySelector()
+const titulo = document.querySelector('.titulo');
+
+// Cambiamos su contenido con innerHTML
+titulo.innerHTML =  titulo.innerHTML + ' Johan'; 
