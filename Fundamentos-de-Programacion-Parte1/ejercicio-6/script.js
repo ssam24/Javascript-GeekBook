46daa23f2ae86ce6e44239d72c71c3bb279ff603
samuel crespo <crespoalzatesamuel@gmail.com>
2020-05-17 16:04:33 -0500

    ejercicio 6 completo

---
+++ Fundamentos-de-Programacion-Parte1/ejercicio-6/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const divElements = document.querySelector(".div");
+
+const content =
+  '<h1>Lorem ipsum</h1><img src="http://via.placeholder.com/350x150"></img><p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit"</p>';
+
+divElements.innerHTML = content;
