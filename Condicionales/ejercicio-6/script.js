af71ce595fbf2fb9b4c3e4ff077ef91989583def
samuel crespo <crespoalzatesamuel@gmail.com>
2020-05-21 16:47:08 -0500

    ejercicio 6

---
+++ Condicionales/ejercicio-6/script.js
@@ -0,0 +1,15 @@
+const success = 'exito';
+let fontColor = (success ==='exito') ? 'green' : 'black';
+let backgroundColor = (success === 'exito') ? '#A7F4A6' : '#F5FEF5';
+let border = (success === 'exito') ? '1px bold green' : 'none';
+
+const error = 'fracaso';
+let fontColor = (error === 'fracaso') ? 'red' : 'black';
+let backgroundColor = (error ===  'fracaso') ? '#F1767F' : '#F5FEF5';
+let border = (error ===  'fracaso') ? '1px bold red' : 'none';
+
+const  warning = 'aviso';
+let fontColor = (warning ==='aviso') ? 'yellow' : 'black';
+let backgroundColor = (warning === 'aviso') ? '#FBFA78 ' : '#F5FEF5';
+let border = (warning === 'aviso') ? '1px bold yellow' : 'none';
+
