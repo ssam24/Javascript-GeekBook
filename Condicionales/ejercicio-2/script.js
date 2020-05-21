f17495c133b3413fb189a9feacab3735ff4918ab
samuel crespo <crespoalzatesamuel@gmail.com>
2020-05-20 19:04:54 -0500

    ejercicio 2

---
+++ Condicionales/ejercicio-2/script.js
@@ -0,0 +1,13 @@
+'use strict' 
+
+let miNombre = " ";
+
+miNombre = prompt("Ingrese su nombre"); 
+
+if (miNombre === "Samuel" ){
+    console.log('Bienvenido, ' + miNombre);
+} else {
+    console.log('Lo siento pero el usuario que has introducido no está registrado')
+
+}
+    
