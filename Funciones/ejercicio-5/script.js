bbb7e873b91b3d06efe5c8182d5f3edfca4ae9e0
samuel crespo <crespoalzatesamuel@gmail.com>
2020-05-21 22:29:49 -0500

    ejercicio 5

---
+++ Funciones/ejercicio-5/script.js
@@ -0,0 +1,11 @@
+a = parseInt(prompt("Ingrese el valor de su compra: "));
+iva = 0.21; //21%
+compra = (a, iva) => {
+
+    const result = (a + (a * iva));
+    return result;
+}
+
+const totalResult = compra(a, iva);
+
+console.log ("El precio sin IVA es: " + a + ", IVA: " + (a * iva) + " y total: " + totalResult);
