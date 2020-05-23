29afd78056eef54845b68347a90d8d181aa92628
samuel crespo <crespoalzatesamuel@gmail.com>
2020-05-22 20:41:47 -0500

    creo carpeta del tema: Funciones y creo el ejercicio 1

---
+++ Eventos/ejercicio-1/script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('.message');
+
+button.addEventListener(
+    'click', () => document.querySelector('p').innerHTML 
+= 'Mi primer click, ¡ole yo y la mujer que me parió!'
+);
