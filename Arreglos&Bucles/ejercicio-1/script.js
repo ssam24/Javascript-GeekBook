c88e44f481c6fcb944e184afc2ee93e617956d3d
samuel crespo <crespoalzatesamuel@gmail.com>
2020-05-24 16:39:11 -0500

    creo carpeta de Arreglos&Bucles y el ejercicio 1

---
+++ Arreglos&Bucles/ejercicio-1/script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+function workWithMovies(){
+const peliculas = [
+    'Harry Potter',
+    'Underworld',
+    'Lord of the Rings',
+];
+
+peliculas[3] = 'Twiligth'; //agrega un elemento al array
+peliculas[2] = 'Maze Runner'; //cambia el elemento 2 del array
+
+console.log(peliculas);
+} 
+
+console.log(workWithMovies());
