456e1cceb6727c0ed1971cdd7d4bebe673e87ec5
samuel crespo <crespoalzatesamuel@gmail.com>
2020-05-23 22:24:50 -0500

    creo carpeta del tema OBJETOS y realizo el ejercicio 1

---
+++ Objetos/ejercicio-1/script.js
@@ -0,0 +1,16 @@
+const geekGirlOne = {
+    name: 'Susana',
+    age: 34,
+    job: 'periodista',
+    
+  };
+  console.log('Mi nombre es,' + geekGirlOne.name +',' + geekGirlOne.job)
+
+  const geekGirlTwo = {
+      name: 'Rocío',
+      age: 25,
+      job: 'actriz',
+  }
+  console.log(
+      `Mi nombre es,${geekGirlTwo.name} ,mi edad es ${geekGirlTwo.age} años y soy ${geekGirlTwo.job}`
+  )
