e7c4358af57bc562d32cd4d54e9cc5b4ef36ecf0
samuel crespo <crespoalzatesamuel@gmail.com>
2020-05-24 12:47:19 -0500

    ejercicio 7

---
+++ Objetos/ejercicio-7/script.js
@@ -0,0 +1,10 @@
+const job = "developer";
+
+const usuario = {
+    firstName: 'Samuel',
+    lastName: 'Crespo',
+    age: 24,
+    job: job,
+}
+
+console.log(`Mi nombre es ${usuario.firstName} ${usuario.lastName}, tengo ${usuario.age} años y trabajo como ${usuario.job}`);
